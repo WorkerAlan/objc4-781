150ad7ec1c58f952df44d45b5dbaadefac831803
Henry <lhappledev@163.com>
2020-09-09 18:41:34 +0800

    init

---
+++ HRTest/HRTest.m
@@ -0,0 +1,12 @@
+//
+//  HRTest.m
+//  HRTest
+//
+//  Created by Henry on 2020/9/9.
+//
+
+#import "HRTest.h"
+
+@implementation HRTest
+
+@end
---
+++ HRTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HRTest
+//
+//  Created by Henry on 2020/9/9.
+//
+
+#import <Foundation/Foundation.h>
+#import "HRTest.h"
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *obj = [NSObject alloc];
+        
+        HRTest * t = [HRTest alloc];
+    }
+    return 0;
+}
